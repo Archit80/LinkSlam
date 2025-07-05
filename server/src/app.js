71349d23027a2b49be1bba47a41dc5f2c9508829
Archit80 <archittaneja80@gmail.com>
2025-07-06 02:28:53 +0530

    npm i

--- server/src/app.js
+++ server/src/app.js
@@ -27,6 +27,7 @@ app.use(
   })
 );
 
+
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "https://link-slam.vercel.app");
 //   res.header("Access-Control-Allow-Credentials", "true");
