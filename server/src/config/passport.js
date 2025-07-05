c5029082f48aa2e78f52822c188a4c44a8256e32
Archit80 <archittaneja80@gmail.com>
2025-07-06 00:50:53 +0530

    fixing OAuth callback

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/google/callback"
+    callbackURL: "https://linkslam.onrender.com/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
