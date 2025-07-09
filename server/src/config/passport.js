82212bd59a8b29b0922661a1465e4cd9a6ae267f
Archit80 <archittaneja80@gmail.com>
2025-07-09 21:01:04 +0530

    OAuth fix

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://linkslam.onrender.com/auth/google/callback"
+    callbackURL: "https://link-slam-x.vercel.app/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
